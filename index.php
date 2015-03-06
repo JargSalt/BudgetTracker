d665ace55b0cecd023fcb6e3c58fd4c42fdc9500
Scott Murrow <smurrow0@gmail.com>
2015-03-06 14:25:25 -0700

    CSS for Login Page
    Added the CSS for the login page. Also, the login page now displays the
    login form only if you are not already logged in. If you are logged in,
    then it asks you if you want to log out, or give you the option to go
    to your home page.

--- index.php
+++ index.php
@@ -14,35 +14,32 @@ if (login_check($mysqli) == true) {
 <html>
     <head>
         <title>Secure Login: Log In</title>
-        <link rel="stylesheet" href="styles/main.css" />
+        <link rel="stylesheet" href="styles/login.css" />
         <script type="text/JavaScript" src="js/sha512.js"></script> 
         <script type="text/JavaScript" src="js/forms.js"></script> 
     </head>
     <body>
+        <div id='header'>
+            <span> Budget Buddy </span>
+        </div>
+        <div id='signin'>
         <?php
         if (isset($_GET['error'])) {
             echo '<p class="error">Error Logging In!</p>';
         }
-        ?> 
-        <form action="includes/process_login.php" method="post" name="login_form">                      
-            Email: <input type="text" name="email" />
-            Password: <input type="password" 
-                             name="password" 
-                             id="password"/>
-            <input type="button" 
-                   value="Login" 
-                   onclick="formhash(this.form, this.form.password);" /> 
-        </form>
- 
-<?php
         if (login_check($mysqli) == true) {
-                        echo '<p>Currently logged ' . $logged . ' as ' . htmlentities($_SESSION['username']) . '.</p>';
- 
-            echo '<p>Do you want to change user? <a href="includes/logout.php">Log out</a>.</p>';
-        } else {
-                        echo '<p>Currently logged ' . $logged . '.</p>';
-                        echo "<p>If you don't have a login, please <a href='register.php'>register</a></p>";
-                }
-?>      
+            echo '<p>Currently logged ' . $logged . ' as ' . htmlentities($_SESSION['username']) . '.</p>';
+            echo '<p>Do you want to change user? <a href="includes/logout.php">Log out</a></p>';
+            echo '<p>Your Page: <a href="protected_page.php">Home</a></p>';
+        }
+        else { 
+            echo '<form action="includes/process_login.php" method="post" name="login_form">'.                      
+                    'Email: <input type="text" name="email" />&nbsp;&nbsp;Password: <input type="password"'. 
+                        'name="password" id="password"/><br><br><input type="button" value="Login"'. 
+                            'onclick="formhash(this.form, this.form.password);" />&nbsp;&nbsp;<input type="button"'.
+                                'value="Register" onclick="location.href = \'register.php\';"></form>';
+        }
+        ?>
+        </div>     
     </body>
 </html>
